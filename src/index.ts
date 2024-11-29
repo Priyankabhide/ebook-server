30b842efa6eb04a0c231e1a3d095f9ef801f47fd
Priyankabhide <bhidepriyanka02@gmail.com>
2024-11-29 15:19:07 +0530

    basic typescript setup

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {add} from '@/test/math'
+const a = 5
+const b = 2
+
+console.log("This is the final result :" ,add(a,b))  // , means 2 separate operations
+console.log("This is the final result :" + a+b)  // + means concatenation
